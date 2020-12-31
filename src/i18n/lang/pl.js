f3de2287021e86a53eba1f8534f2ab001cf2084f
Tomasz Myśliwiec <tomek@mysliwiec.pro>
2020-12-31 12:08:20 +0100

    Add main text

--- src/i18n/lang/pl.js
+++ src/i18n/lang/pl.js
@@ -1,117 +1,148 @@
 export const pl = {
-  application_name: 'PayTip',
-  main: 'Główna',
-  for_who: 'dla kogo',
-  why_worth: 'dlaczego warto',
-  why_worth_question: 'Dlaczego warto?',
-  how_works: 'jak to działa',
-  how_works_question: 'Jak to działa?',
-  safety: 'bezpieczeństwo',
-  opinions: 'opinie',
-  revolution_in_word: 'Rewolucja w świecie',
-  tip_header: 'napiwków!',
-  dont_waste_your_tips: 'Nie trać napiwków gdy nie ma gotówki',
-  safe_transactions: 'Bezpieczne transakcje',
-  instant_money: 'Środki natychmiast na koncie PayTip',
-  paytip_for_who: 'Dla kogo jest PayTip?',
-  test_paytip: 'Przetestuj PayTip',
-  follow_the_instructions_mobile: 'Kliknij w wybrany przycisk jeżeli chcesz wręczyć bądź przyjąć napiwek.',
-  follow_the_instructions_desktop: 'Zeskanuj kod QR, podążaj za instrukcjami i wręcz lub obierz swój pierwszy napiwek!',
-  check: 'Sprawdź',
-  try_it: 'Wypróbuj',
-  for_who_paytip_is: 'Dla kogo jest PayTip?',
-  more: 'Więcej',
-  waiters: 'Kelnerzy',
-  and_other_services: 'i inni przedstawiciele branży usługowej',
-  clients: 'Klienci',
-  wants_to_paytip: 'którzy chcą dać napiwek ale nie mają gotówki',
-  to_top: 'Do góry',
-  wy_worth_explain: `
-    <strong>PayTip</strong> jest rewolucyjną usługą PWA, która pozwala na wręczanie i otrzymywanie napiwków w dowolnym miejscu i momencie, 
-    <strong>całkowicie bezgotówkowo!</strong> Osoby przyjmujące napiwek mają pełną kontrolę nad otrzymywanymi środkami. Zawsze wiedzą ile pieniędzy trafia 
-    na ich konto i co najważniejsze, napiwki trafiają <strong>zawsze natychmiast i bezpośrednio na konto</strong> użytkownika. Dodatkowo wszystkie 
-    transakcje <strong>są anonimowe</strong>, co zwiększa komfort obydwu stron transakcji.
-  `,
-  easy_in_use: 'Łatwa obsługa',
-  quick_transactions: 'Szybkie transakcje bezgotówkowe',
-  free_access: 'Swobodny dostęp do środków',
-  full_control: 'Pełna kontrola napiwków',
-  how_works_explain: `
-    Sposób działania <strong>PayTip</strong> jest wyjątkowo prosty. Czasem wystarczą dosłownie dwa kroki, żeby wręczyć napiwek! Dbając o najwyższy komfort 
-    użytkowników pomyśleliśmy o różnych formach przyjmowania napiwków. Dzięki temu, jeżeli ktoś sobie ceni "Tipową dyskrecję", czy zagaduje klientów 
-    "jeżeli szanowny Pan sobie życzy" będzie bardzo zadowolony z tego co oferuje PayTip. Aplikację cechuje duża elastyczność, dzięki czemu 
-    <strong>nie musisz zmieniać swoich nawyków napiwkowych!</strong> :)
-  `,
-  know_more: 'Dowiedz się więcej',
-  giving_tip: 'Wręczam napiwek',
-  receive_tip: 'Przyjmuję napiwek',
-  scan_qr_code: 'zeskanuj<br/>kod QR',
-  give_tip_amount: 'podaj wysokość<br/>napiwku',
-  give_tip_in_comfortable_way: 'przekaż napiwek w wygodny<br/>dla Ciebie sposób',
-  why_qr_code: 'Dlaczego kod QR?',
-  signin_in_app: 'Zarejestruj się w aplikacji',
-  give_amount_and_tip_height: 'Podaj kwotę rachunku i wybierz wysokość napiwku',
-  receive_tip_in_comfortable_way: 'Odbierz napiwek w wygodny dla Ciebie sposób',
-  knf_supervision: 'Podmiot nadzorowany przez',
-  safety_explain: `
-    Wiemy jak ważne jest dla Ciebie <strong>bezpieczeństwo transakcji</strong>. Dlatego z najwyższą starannością zadbaliśmy o bezpieczeństwo na 
-    każdym etapie transakcji <strong>PayTip</strong>. 
-    Dodatkowo, jesteśmy pod stałą kontrolą Komisji Nadzoru Finansowego, która weryfikuje przepływy finansowe przez apkę <strong>PayTip</strong>.
-  `,
-  application_is_marvelous: 'Aplikacja jest rewelacyjna!',
-  problem_with_cash: 'Problem z brakiem gotówki wśród klientów naszej restauracji był coraz bardziej dotkliwy.',
-  piotr_waiter: 'Piotr, kelner',
-  i_looking_this: 'PayTip to jest to czego szukałem',
-  i_dont_know_qr: 'Nie wiedziałem, że można tak po prostu pozostawić wizytówkę z kodem QR i napiwek sam wpada! :)',
-  jan_food_delivery: 'Jan, dostawca jedzenia',
-  my_clients_and_i: 'Moje klientki są zachwycone. I ja też',
-  clients_leave_tips: 'Odkąd jest PayTip nasze klientki zaczęły zostawiać napiwki.',
-  anna_nails_stylist: 'Anna, stylistka paznokci',
-  contact_info: `
-    Jesteś przedsiębiorcą?
-    <a href="&#109;&#97;&#105;&#108;&#116;&#111;&#58;&#105;&#110;&#102;&#111;&#64;&#112;&#97;&#121;&#116;&#105;&#112;&#46;&#105;&#111;" style="color: #404040">Skontaktuj się z nami</a>
-    i dowiedz się jakie korzyści płyną ze wsparcia PayTip u Ciebie.
-  `,
-  terms: 'Regulamin',
-  privacy_policy: 'Polityka prywatności',
-  knf_number: 'Wpis do rejestru KNF numer: MIP47/2019',
-  address_part1: 'XCFT Sp. z o.o. - Aleje Jerozolimskie 100,',
-  address_part2: 'Warszawa 00-807',
-  all_rights_reserved: 'Wszelkie prawa zastrzeżone',
-  thanks_for_paytip: 'Dzięki PayTip nie uciekają mi napiwki gdy klient nie ma gotówki',
-  waiter_more: `
-    Jesteś kelnerem, fryzjerem lub innym pracownikiem branży usługowej i brak gotówki u klientów coraz bardziej Ci doskwiera? Zarejestruj się w <strong>PayTip</strong> i 
-    uwolnij się od gotówki! Zbieraj napiwki, zarządzaj nimi i przelewaj na swoje konto w dowolnym momencie, gdy tylko chcesz. A jeżeli masz mniej niż 26 lat, 
-    Twoje transakcje są zupełnie bez podatku!
-  `,
-  make_account: 'Załóż konto',
-  back: 'Powrót',
-  try: 'Wypróbuj',
-  client_more_header: 'Dzięki PayTip mogę dać napiwek, nawet gdy nie mam gotówki.',
-  client_more: `
-    Jesteś w restauracji i zachwyciło Cię zaserwowane jedzenie? A może chcesz dać napiwek człowiekowi na stacji benzynowej, który właśnie zatankował Ci samochód? 
-    Uliczny grajek zagrał melodię, która poruszyła Twoim sercem? <strong>I nie masz przy sobie gotówki?</strong> Od teraz nie ma to żadnego znaczenia. 
-    Możesz wręczyć napiwek każdemu i w dowolnym momencie.
-  `,
-  why_qr_part1: `
-    <strong>Dlaczego kod QR?</strong> Bo to najwygodniejsza forma komunikacji pomiędzy ludźmi, którzy się nie znają i urządzeniami, które z różnych przyczyn mogą sobie nie ufać.
-  `,
-  why_qr_part2: `
-    Transakcje za pośrednictwem kodu QR <strong>są wyjątkowo bezpieczne</strong> i zdecydowanie szybsze niż klasyczne przelewy. Kod QR jest również dyskretną formą, którą można w niezobowiązujący sposób pozostawić do indywidualnej decyzji klienta.
-  `,
-  why_qr_part3: `
-    Praktycznie każde nowe urządzenie ma wbudowany <strong>czytnik kodów QR w aplikacji aparatu</strong>. Wystarczy ją uruchomić,najechać na kod i podążyć za linkiem! :)
-  `,
-  fold: 'Zwiń',
-  cookie_info: 'Nasza strona internetowa używa plików cookies (tzw. ciasteczka) w celach statystycznych, reklamowych oraz funkcjonalnych. Dzięki nim możemy indywidualnie dostosować stronę do twoich potrzeb. Każdy może zaakceptować pliki cookies albo ma możliwość wyłączenia ich w przeglądarce, dzięki czemu nie będą zbierane żadne informacje.',
-  got_it: 'Rozumiem',
-  type_amount: 'Wpisz kwotę napiwku jaką chcesz przekazać.',
-  generated_qr_code: 'Wygenerowany kod QR pokaż kelnerowi w celu zeskanowania.',
-  waiter_on_device: 'Kelner na swoim urządzeniu odbierze napiwek.',
-  promo_terms: 'Regulamin promocji',
-  give_tip: 'wręcz napiwek',
-  receive_tip_form: 'przyjmij napiwek',
-  start_using_paytip: 'Zacznij korzystać z PayTip',
-  give: 'Wręcz',
-  receive: 'Przyjmij'
+  'sendOk': 'Ok! Dzięki.',
+  'sendError': 'Ups... Błąd!',
+  'menu': {
+    'main': 'START',
+    'about': 'O MNIE',
+    'portfolio': 'PORTFOLIO',
+    'contact': 'KONTAKT'
+  },
+  'main': {
+    mainText1: 'Witaj.',
+    mainText2: 'Jestem Web Develeoperem /',
+    mainText3: 'Front-end Developerem.',
+    mainText4: 'Zajmuję się kodowaniem stron',
+    mainText5: 'i aplikacji internetowych.'
+  },
+  'contact': {
+    'description': '<p>Aktualnie mieszkam w Zielonce blisko Warszawy.</p><p>Jeśli chcesz się ze mną skontaktować użyj proszę przycisków w stopce strony.</p>',
+    'name': 'Twoje imię',
+    'email': 'Adres Email',
+    'subject': 'Temat',
+    'defaultSubject': 'Nic specjalnego',
+    'message': 'Wiadomość',
+    'submit': 'Wyślij',
+    'minimumError': 'Minimum 3 znaki',
+    'emailError': 'Zły adres'
+  },
+  'about': {
+    'introText': '<p>Stronami internetowymi zajmuję się od czasów szkoły średniej, czyli gdzieś od 1996r. Wtedy właśnie siedziałem po nocach przed ekranem monitora i wpatrywałem się w dziwne i niezrozumiałe (dla wielu) tagi języka HTML. Oczywiście nie muszę wspominać, że przerodziło się to w moją pasję. Dzięki tej pasji później ukończyłem studia informatyczne i do dziś uwielbiam kodować w językach używanych do tworzenia stron internetowych.<p><p>Obecnie pracuję jako Front-End Team Leader w firmie <a href=\'https://www.execon.pl/\'>Execon ITS</a>.</p>',
+    'headerCoding': 'Koduję wwwy:)',
+    'headerFrameworks': 'Używane frameworki i biblioteki',
+    'headerTechnologies': 'Używane technologie',
+    'headerCMS': 'Znane CMS\'y',
+    'headerSoftware': 'Używane oprogramowanie',
+    'introSiteHeader': 'Stos technologiczny strony',
+    'introSiteText': '<p>Do strworzenia tej strony wykorzystałem technologie, frameworki, skrypty i biblioteki takie jak:</p><ul><li>HTML</li><li>Sass - Na stronie nie został użyty żaden plik graficzny (z wyjątkiem portfolio)</li><li>BEM</li><li>RWD - Mobile First</li><li>JavaScript</li><li>Angular JS</li><li>jQuery</li><li>PHP - tylko do wysyłania formularza kontaktowego bo obecny hosting nie obsługuje node.js:(</li><li>JSON - teksty polsko i angielsko języczne</li><li>textillate.js - efekt tekstu na stronie głównej</li><li>lettering.js - wymagane przez textillate.js</li><li>enter-view.js</li><li>Google Fonts - Jura</li><li>Visual Studio Code</li><li>GIT</li><li>GitHub - repozytorium z moją stroną dostępne tu: <a href=\'https://github.com/tomekmy\'>github.com/tomekmy</a></li><li>Webpack</li><li>Chrome Developer Tools</li></ul>',
+    'introCertificatesHeader': 'Certyfikaty',
+    'introCertificate': [{
+      'link': 'https://www.udemy.com/certificate/UC-Q0YJQIKU/',
+      'description': 'JavaScript: Understanding the Weird Parts'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-Y44IJ397/',
+      'description': 'Essentials in JavaScript ES6 - A Fun and Clear Introduction'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-05ARETD8/',
+      'description': 'Quickstart AngularJS'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-44RMZD7S/',
+      'description': 'Git Started with GitHub'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-15P7OZMB/',
+      'description': 'Sass Workflow'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-N657IK98/',
+      'description': 'Learn and Understand AngularJS'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-BHQ717YM/',
+      'description': 'Learn Webpack 2 from scratch'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-RVYOH4ON/',
+      'description': 'Angular 6 (formerly Angular 2) - The Complete Guide'
+    },
+    {
+      'link': 'https://mysliwiec.pro/assets/bottega_react_certyfikat.pdf',
+      'description': 'Reaktywne programowanie aplikacji webowych w oparciu o React.js, Redux i ES6',
+      'target': '_blank'
+    },
+    {
+      'link': 'https://www.udemy.com/certificate/UC-QA3KAOPL/',
+      'description': 'React - The Complete Guide'
+    }
+    ]
+  },
+  'portfolio': {
+    'online': {
+      'header': 'Prace online',
+      'works': [{
+        'title': 'Wystaw śmieci',
+        'url': 'zielonka.wystaw-smieci.pl',
+        'description': '<p>Aplikacja PWA przypominająca o terminach odbioru nieczystości w mieście Zielonka. Aplikacja pozwala na przegląd terminów odbioru śmieci oraz umożliwia włączanie i wyłączanie powiadomień dla poszczególnych sektorów.</p><p>Apka została napisana w Angular 5 oraz zintegrowana z <a href=\'https://onesignal.com/\'>OneSignal</a> API.</p>',
+        'img': 'img/wystaw-smieci-pwa'
+      },
+      {
+        'title': 'Wystaw śmieci',
+        'url': 'wystaw-smieci.pl',
+        'description': '<p>Landing Page aplikacji powiadamiającej o terminach odbioru śmieci w mieście Zielonka. Aplikacja przeznaczona jest dla systemów Android i oprócz powiadomień umożliwia przegląd terminów odbiorów nieczystości.</p><p>W przyszłości na miejscu tej strony powstanie odpowiednik webowy tej aplikacji.</p><p>W projekcie wykorzystałem: <a href=\'https://github.com/vuetifyjs/pwa\'>vuetify.js</a></p>',
+        'img': 'img/wystaw-smieci-portfolio'
+      },
+      {
+        'title': 'TechCalc - Pomocnik Inżyniera',
+        'url': 'www.techcalc.pl',
+        'description': '<p>Aplikacja internetowa dla inżynierów, technologów, konstruktorów, zawierająca dużo przydatnych dla nich narzędzi i materiałów.</p><p>Obecną wersję strony oparłem o CMS Wordpress a wszystkie przeliczniki i narzędzia napisałem w JavaScript z jQuery.</p>',
+        'img': 'img/techcalc-portfolio'
+      },
+      {
+        'title': 'Centrum Technologiczne Warszawa',
+        'url': 'www.centrumtechnologiczne.pl',
+        'description': '<p>Centrum Technologiczne jest międzynarodową korporacją z ponad 20-letnim doświadczeniem w projektowaniu i produkcji maszyn i form do przetwórstwa tworzyw sztucznych.</p><p>Strona oparta na CMS Joomla. Większość zdjęć na stronie wykonał jeden z moich klientów – <a href=\'http://www.lagocki.pl/\'>Wojciech Łagocki</a>.</p>',
+        'img': 'img/centrumtechnologiczne-portfolio'
+      },
+      {
+        'title': 'BluePhoto | Ireneusz Rek Fotografia',
+        'url': 'www.bluephoto.pl',
+        'description': '<p>Ireneusz Rek - reportaż, dokument, fotograficzne spostrzeżenia autora. Portfolio autora, wybór zdjęć z wykonanych prac jak i relacje z podróży, wydarzeń.</p><p>Na stronie wykorzystałem CMS Wordpress.</p>',
+        'img': 'img/bluephoto-portfolio'
+      },
+      {
+        'title': 'Wojciech Łagocki - Fotografia',
+        'url': 'www.lagocki.pl',
+        'description': '<p>Wojciech Łagocki pasja - Fotografia. Ulubione tematy: fotografia ślubna fotografia niemowlaków noworodków fotografia dziecięca fotografia rodzinna portret.</p><p>Strona napisana przy użyciu JavaScript, jQuery, PHP w technologii SPA.</p>',
+        'img': 'img/wojtek-portfolio'
+      },
+      {
+        'title': 'KMP Plastics - maszyny i urządzenia peryferyjne dla Przetwórstwa Tworzyw Sztucznych.',
+        'url': 'www.kmpplastics.com.pl',
+        'description': '<p>KMP Plastics - oferuje maszyny i urządzenia peryferyjne dla Przetwórstwa Tworzyw Sztucznych.</p><p>Jeden z moich starszych projektów oparty o CMS Joomla</p>',
+        'img': 'img/kmp-portfolio'
+      }
+      ]
+    },
+    'offline': {
+      'header': 'Prace offline',
+      'works': [{
+        'title': 'Maciej Majchrzak - Director of photography',
+        'description': '<p>Strona Macieja Majchrzaka - operatora kamery filmowej.</p><p>Stronę napisałem z wykorzystaniem HTML5, JavaScript, PHP, MySQL. Na tle strony umieściłem film autorstwa Macieja. Film osadzono przy użyciu HTML5 video. Stronę zintegrowano z serwisem YouTube za pomocą prostego CMS.</p>',
+        'img': 'img/maciejmajchrzak-portfolio.jpg'
+      },
+      {
+        'title': 'Skrawek Nieba - Domki letniskowe. Dąbki',
+        'description': '<p>Strona domków letniskowych usytuowanych nad Polskim morzem w Dąbkach obok Darłowa.</p><p>Strona oparta o CMS Joomla. Projekt graficzny oraz animacja Flash: Maciej Burkowski.</p>',
+        'img': 'img/skraweknieba-portfolio.jpg'
+      },
+      {
+        'title': 'Usługi i Szkolenia BHP. Kursy Pierwszej Pomocy',
+        'description': '<p>Strona Janusza Gawrycha właściciela firmy GJTECH. GJTECH jest firmą szkoleniowo - doradczą specjalizującą się w dziedzinie Bezpieczeństwa i Higieny Pracy oraz zaopatrywania w artykuły BHP, PPOŻ i Pierwszej Pomocy.</p><p>Stronę napisałem z wykorzystaniem JavaScript, PHP, MySQL. Strona posiada prosty system CMS.</p>',
+        'img': 'img/bhp-portfolio.jpg'
+      }
+      ]
+    }
+  }
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,66 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Flip from 'react-reveal/Flip';
 
 import Layout from '../components/layout/layout';
 import SEO from '../components/seo/seo';
 
+import styles from './index.module.scss';
+
 const IndexPage = () => {
+  const { t } = useTranslation();
 
   return (
     <Layout>
       <SEO title="Home" />
-      Home
+      <div className={styles.home__text}>
+        <div className={styles.home__textParagraph}>
+          <Flip
+            left
+            cascade
+            delay={50}
+            duration={450}
+          >
+            {t('main.mainText1')}
+          </Flip>
+        </div>
+        <div className={styles.home__textParagraph}>
+          <Flip
+            left
+            cascade
+            delay={450}
+            duration={4000}
+          >
+            {t('main.mainText2')}
+          </Flip>
+          <Flip
+            left
+            cascade
+            delay={2000}
+            duration={1000}
+          >
+            {t('main.mainText3')}
+          </Flip>
+        </div>
+        <div>
+          <Flip
+            left
+            cascade
+            delay={450}
+            duration={4000}
+          >
+            {t('main.mainText4')}
+          </Flip>
+          <Flip
+            left
+            cascade
+            delay={2000}
+            duration={1000}
+          >
+            {t('main.mainText5')}
+          </Flip>
+        </div>
+      </div>
     </Layout>
   );
 };
